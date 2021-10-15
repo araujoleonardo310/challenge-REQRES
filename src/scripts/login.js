16b0021b30802a1064a56179a812691e7e4968bf
araujoleonardo310 <araujoleonardo310@gmail.com>
2021-10-15 10:45:40 -0300

    Capturado dados e enviado para api

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -2,17 +2,18 @@ const api = 'https://reqres.in/api';
 
 const submit = document.getElementById('submit');
 submit.addEventListener("click", getValores);
+
 async function getValores() {
 
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    var data = {
+    var dados = {
         email: email,
         password: password
-    };   
-
-    await axios.post(`${api}/login`, data)
-
+    };
     
+    await axios.post(`${api}/login`, dados)   
 }
+
+
