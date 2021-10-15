12ae768afcacc8040c1d419f8af116a41b0a8287
araujoleonardo310 <araujoleonardo310@gmail.com>
2021-10-15 13:45:09 -0300

    dados aparecendo na tela

---
+++ src/scripts/users.js
@@ -0,0 +1,37 @@
+const api = 'https://reqres.in/api/users?page=2';
+
+const divContainer = document.querySelector(".container-users")
+
+const GetUsers = async () => {
+    var responseUsers = await axios.get(`${api}`);
+    console.log(responseUsers)
+    ListUsers(responseUsers.data.data)            
+    
+        
+}
+
+GetUsers();
+
+function ListUsers(item){
+    let usuarios = "";
+    item.forEach(element => {
+        usuarios +=
+        `
+        <div class="users">
+                <div class="users-profile">
+                    <img src="${element.avatar}" alt="" class="photo">
+                    <div class="users-name_email">
+                        <p class="name">${element.first_name} ${element.last_name}</p>
+                        <p class="email">${element.email}</p>
+                    </div>
+                </div>
+                <button class="button" id="button">Visite</button>            
+            </div>
+        `
+        divContainer.innerHTML = usuarios
+    });
+}
+
+
+
+
