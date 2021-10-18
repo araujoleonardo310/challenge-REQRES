b18846dca31b8656c784f1f56c651077b93bbfa0
araujoleonardo310 <araujoleonardo310@gmail.com>
2021-10-18 11:55:05 -0300

    modal concluído e funcionando

--- src/scripts/users.js
+++ src/scripts/users.js
@@ -1,6 +1,7 @@
 const api = 'https://reqres.in/api/users?page=2';
 const divContainer = document.querySelector(".container-users");
 
+
 const GetUsers = async () => {
     var responseUsers = await axios.get(`${api}`);
     ListUsers(responseUsers.data.data)                    
@@ -14,12 +15,11 @@ function ListUsers(item) {
     `
     <div class="users-title">
         <p class="title"><i class="fas fa-users"></i>Usuários</p>     
-    </div>   
-    
+    </div>      
     `
-
     let usuarios = "";
     item.forEach(element => {
+        const name = `${element.first_name} ${element.last_name}`;        
         usuarios +=
         `
         <div class="users">
@@ -30,14 +30,29 @@ function ListUsers(item) {
                     <p class="email">${element.email}</p>
                 </div>
             </div>
-            <button class="button" id="button">Visite</button>            
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@${element.email}">Enviar @${element.first_name}</button>          
         </div>
         `        
         divContainer.innerHTML = div + usuarios;
-    });
+    })
 }
 
+var exampleModal = document.getElementById('exampleModal')
+exampleModal.addEventListener('show.bs.modal', function (event) {
+  // Button that triggered the modal
+  var button = event.relatedTarget
+  // Extract info from data-bs-* attributes
+  var recipient = button.getAttribute('data-bs-whatever')
+  // If necessary, you could initiate an AJAX request here
+  // and then do the updating in a callback.
+  //
+  // Update the modal's content.
+  var modalTitle = exampleModal.querySelector('.modal-title')
+  var modalBodyInput = exampleModal.querySelector('.modal-body input')
 
+  modalTitle.textContent = 'Nova mensagem'
+  modalBodyInput.value = recipient
+})
 
 
 
